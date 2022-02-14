9597670b6d6d2ad02113fe8384290ce390ea470e
Sahib Singh <75523820+ssahibsingh@users.noreply.github.com>
2022-02-14 16:45:23 +0530

    updated PORT

--- app.js
+++ app.js
@@ -107,6 +107,11 @@ app.get("/posts/:postId", function (req, res) {
   })
 });
 
-app.listen(3000, function () {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function () {
   console.log("Server started on port 3000");
 });
